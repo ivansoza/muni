1bf585c8ec155e94b7c0177f183b7a532ddab93d
ivansoza <ivansz123@gmail.com>
2025-01-30 20:14:05 -0600

    `Updated navigation menu and sidebar activation logic in templates and views`

--- muni/home/views.py
+++ muni/home/views.py
@@ -5,3 +5,8 @@ from django.views.generic.base import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'home/index.html'  # Ruta del template
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['sidebar'] = 'home'  # Marcar 'Inicio' como activo
+        return context
--- muni/servicios/views.py
+++ muni/servicios/views.py
@@ -4,3 +4,8 @@ from django.views.generic.base import TemplateView
 # Create your views here.
 class HomeServiciosView(TemplateView):
     template_name = 'homeServicios.html'  # Ruta del template
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['sidebar'] = 'servicios'  # Define qué opción está activa
+        return context
