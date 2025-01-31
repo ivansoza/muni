160f2a437697b69e800d862c2df6cb618b40503f
ivansoza <ivansz123@gmail.com>
2025-01-30 20:48:33 -0600

    `Update navigation menu and noticias view to link to homeNoticias URL`

--- muni/noticias/views.py
+++ muni/noticias/views.py
@@ -4,3 +4,9 @@ from django.views.generic.base import TemplateView
 # Create your views here.
 class HomeNoticiasView(TemplateView):
     template_name = 'homeNoticias.html' 
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['sidebar'] = 'noticias'  # Define qué opción está activa
+        return context
