aaa997f72fa59c9d7f55be3c6b65f385586cd916
arley ivan solis zacapantzi <ivansz123@gmail.com>
2025-02-07 23:05:24 -0600

    `Updated logout URL and added logout form in header.html`

--- muni/generales/urls.py
+++ muni/generales/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("", CustomLoginView.as_view(),name='login'),
-    path('logout/', LogoutView.as_view(next_page='homepage'),  name='logout'),      
+    path('logout/', LogoutView.as_view(next_page='home'),  name='logout'),      
     path('mi-panel/', DashboardView.as_view(), name='dashboard'),
 
 
