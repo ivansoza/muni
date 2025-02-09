a36a03c67b3044daa3b060cf4468aee4f07b5f99
ivansoza <ivansz123@gmail.com>
2025-02-08 23:41:49 -0600

    `Added PersonalizacionView and updated templates to display breadcrumb and sidebar for personalization page`

--- muni/generales/urls.py
+++ muni/generales/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
-from .views import CustomLoginView, DashboardView
+from .views import CustomLoginView, DashboardView, PersonalizacionView
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("", CustomLoginView.as_view(),name='login'),
     path('logout/', LogoutView.as_view(next_page='home'),  name='logout'),      
     path('mi-panel/', DashboardView.as_view(), name='dashboard'),
+    path('personalizacion/', PersonalizacionView.as_view(), name='personalizacion_view'),
 
 
 
--- muni/generales/views.py
+++ muni/generales/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.views import LoginView
 from django.contrib import messages
 from django.shortcuts import redirect
+from django.urls import reverse
 
 from django.urls import reverse_lazy
 
@@ -33,6 +34,22 @@ class DashboardView(LoginRequiredMixin, TemplateView):
         context['breadcrumb'] = {
             'parent': {'name': 'Dashboard', 'url': '/index'},
             'child': {'name': 'Home comisión de agua potable'}
+        }
+        context['sidebar'] = 'inicio'
+
+        return context
+    
+
+class PersonalizacionView(LoginRequiredMixin, TemplateView):
+    template_name = "personalizacion.html"  
+    login_url = reverse_lazy('login')  # Redirigir si no está autenticado
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        url_configuracion = reverse('dashboard')
+        context["breadcrumb"] = {
+            'parent': {'name': 'Dashboard', 'url': url_configuracion},
+            'child': {'name': 'Personalizar', 'url': ''}
         }
         context['sidebar'] = 'inicio'
 
