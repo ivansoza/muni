a8d99d1457555c8ca44abd30bc15f79614129efd
ivansoza <ivansz123@gmail.com>
2025-02-13 10:07:47 -0600

    `Added functionality to save colors for a municipality and updated code to reflect changes in the UI and backend.`

--- muni/informacion_municipal/urls.py
+++ muni/informacion_municipal/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from generales.views import get_municipio_data
-from .views import HomeInformacionView
+from .views import HomeInformacionView, guardar_colores
 
 urlpatterns = [
     path('', HomeInformacionView.as_view(), name='homeInformacion'), 
     path('ajax/municipio/', get_municipio_data, name='ajax_municipio_data'),
+    path('ajax/guardar-colores/', guardar_colores, name='guardar_colores'),
 
 ]
