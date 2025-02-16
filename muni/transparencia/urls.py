bf049db52ed95a2bd551db894e64ae861577da69
ivansoza <ivansz123@gmail.com>
2025-02-16 16:52:54 -0600

    `Updated home page to link to reportes page and added new view for reportes page`

--- muni/transparencia/urls.py
+++ muni/transparencia/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import HomeTransparenciaView
+from .views import HomeReportesView, HomeTransparenciaView
 
 urlpatterns = [
     path('', HomeTransparenciaView.as_view(), name='homeTransferencia'), 
+
+    path('/reportes', HomeReportesView.as_view(), name='homeReportes'), 
+
+
+    
 ]
--- muni/transparencia/views.py
+++ muni/transparencia/views.py
@@ -4,3 +4,9 @@ from django.views.generic.base import TemplateView
 # Create your views here.
 class HomeTransparenciaView(TemplateView):
     template_name = 'homeTransparencia.html' 
+
+
+
+
+class HomeReportesView(TemplateView):
+    template_name = 'reportes/reportes.html' 
