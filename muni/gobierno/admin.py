7ad94750b8ecb0a2d5da1b44ca469748c8ccf286
arley ivan solis zacapantzi <ivansz123@gmail.com>
2025-02-19 17:59:01 -0600

    cfgvhjknlm

--- muni/gobierno/admin.py
+++ muni/gobierno/admin.py
@@ -1,3 +1,18 @@
+# admin.py
 from django.contrib import admin
+from .models import  MiembroGabinete
 
-# Register your models here.
+class MiembroGabineteInline(admin.TabularInline):
+    model = MiembroGabinete
+    extra = 1
+    fields = ('orden', 'nombre', 'cargo', 'status')
+    readonly_fields = ()
+    ordering = ('orden',)
+
+
+@admin.register(MiembroGabinete)
+class MiembroGabineteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'cargo', 'municipio', 'orden', 'status')
+    list_filter = ('municipio', 'status')
+    ordering = ('orden',)
+    search_fields = ('nombre', 'cargo')
