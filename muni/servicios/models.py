1aafa85ba57c8589002affbaecf5fa7386a206fb
Adair Gustavo <adairgmh@gmail.com>
2025-02-21 23:41:57 -0600

    Modelos y home

--- muni/servicios/models.py
+++ muni/servicios/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.nombre
+    
+    class Meta:
+        verbose_name_plural = "Categorías"
+    
+class Sector(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.nombre
+    
+    class Meta:
+        verbose_name_plural = "Sectores"
+    
+class Dependencia(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.nombre
+    
+    class Meta:
+        verbose_name_plural = "Dependencias"
+    
+class Estado(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.nombre
+    
+    class Meta:
+        verbose_name_plural = "Estados"
+    
+class Servicio(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    icono = models.ImageField(upload_to='iconos/', null=True, blank=True)  # Permite subir un icono
+    url_tramite = models.URLField(null=True, blank=True)
+    estado = models.PositiveIntegerField(default=0)  # Número para "+5" en la tarjeta
+    clasificacion = models.ForeignKey(Categoria, on_delete=models.PROTECT)
+    sector = models.ForeignKey(Sector, on_delete=models.PROTECT)
+    organismo = models.ForeignKey(Dependencia, on_delete=models.PROTECT)
+    creado_en = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
+    
+    class Meta:
+        verbose_name_plural = "Servicios"
