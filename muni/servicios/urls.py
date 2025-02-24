c16ab29f5b3d1d34296c0b2c942bb54759d5303d
ivansoza <ivansz123@gmail.com>
2025-02-24 08:03:23 -0600

    wfewffef

--- muni/servicios/urls.py
+++ muni/servicios/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import HomeServiciosView, ServicioDetailView
 
 urlpatterns = [
-    path('', HomeServiciosView.as_view(), name='homeServicios'),  # Homepage
+    path('', HomeServiciosView.as_view(), name='homeServicios'), 
     path('<uuid:pk>/', ServicioDetailView.as_view(), name='detalle_servicio'),
 ]
