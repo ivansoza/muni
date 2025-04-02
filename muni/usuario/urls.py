780d855d5530d22cfb3ab083a9e35a504eb585b8
ivansoza <ivansz123@gmail.com>
2025-04-02 11:00:43 -0600

    `Added password change functionality to user profile page`

--- muni/usuario/urls.py
+++ muni/usuario/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
-from .views import PerfilUsuarioView
+from .views import CustomPasswordChangeDoneView, CustomPasswordChangeView, PerfilUsuarioView
 
 urlpatterns = [
     path('mi-perfil/', PerfilUsuarioView.as_view(), name='perfil_usuario'),
+    path(
+        'cambiar-contraseña/',
+        CustomPasswordChangeView.as_view(),
+        name='password_change'
+    ),
 
+    # Confirmación de cambio de contraseña exitoso
+    path(
+        'cambiar-contraseña/hecho/',
+        CustomPasswordChangeDoneView.as_view(),
+        name='password_change_done'
+    ),
 ]
--- muni/usuario/views.py
+++ muni/usuario/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 from django.urls import reverse
+from django.contrib.auth.views import PasswordChangeView
+from django.contrib.auth.views import PasswordChangeDoneView
+
+from django.urls import reverse_lazy
+
+from usuario.forms import NoAutofocusPasswordChangeForm
 
 # Create your views here.
 
@@ -23,3 +29,25 @@ class PerfilUsuarioView(LoginRequiredMixin, TemplateView):
         context['usuario'] = self.request.user
 
         return context
+    
+
+class CustomPasswordChangeView(LoginRequiredMixin, PasswordChangeView):
+    form_class = NoAutofocusPasswordChangeForm
+    template_name = 'registration/cambiar_contraseña.html'
+    success_url = reverse_lazy('password_change_done')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context["breadcrumb"] = {
+            'parent': {'name': 'Mi Perfil', 'url': '/admin'},
+            'child': {'name': 'Cambiar Contraseña', 'url': ''}
+        }
+        url_configuracion = reverse( 'perfil_usuario')
+        context['regreso_url']= url_configuracion
+
+        return context
+    
+
+class CustomPasswordChangeDoneView(LoginRequiredMixin, PasswordChangeDoneView):
+    template_name = 'registration/cambiar_contraseña_hecho.html'
