fc0910a091940066790ee5784df2a123c63b4ae8
Adair Gustavo Montiel Hernandez <111458280+adairgmh@users.noreply.github.com>
2025-04-13 23:26:19 -0600

    Update settings.py
    Reparacion de lineas dobles en el settings

--- muni/muni/settings.py
+++ muni/muni/settings.py
@@ -269,7 +269,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'  # URL base para archivos estáticos
 STATICFILES_DIRS = [BASE_DIR / "static"]  # Carpeta de archivos estáticos
 STATIC_ROOT = BASE_DIR / "staticfiles"  # Carpeta para archivos estáticos recolectados (opcional)
 MEDIA_URL = '/media/'
@@ -278,7 +277,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
