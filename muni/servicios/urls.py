4ac8b900fdc3eb951723ac70bf0de16f76149d2c
Adair Gustavo <adairgmh@gmail.com>
2025-04-30 14:20:47 -0600

    Vista para detalle parcial ajax

--- muni/servicios/urls.py
+++ muni/servicios/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomeServiciosView, ServicioDetailView
+from .views import HomeServiciosView, ServicioDetailView, ServicioDetalleParcialView
 
 urlpatterns = [
     path('', HomeServiciosView.as_view(), name='homeServicios'), 
     path('<uuid:pk>/', ServicioDetailView.as_view(), name='detalle_servicio'),
+    path('<uuid:pk>/detalle_parcial/', ServicioDetalleParcialView.as_view(), name='detalle_servicio_parcial'),
 ]
