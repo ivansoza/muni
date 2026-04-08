9ee0b4ecbc3689f9123f389b62d80a5fc09278dd
agmh_prod <adairgmh@gmail.com>
2026-04-08 04:14:42 +0000

    Revertir cambios posteriores a 99d6b72

--- muni/transparencia/admin.py
+++ muni/transparencia/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Encuesta, Pregunta, Opcion, Envio, Respuesta
-from .models import ListaObligaciones, ArticuloLiga,LigaArchivo, CarpetaTransparencia
+from .models import ListaObligaciones, ArticuloLiga,LigaArchivo
 
 # Inline para editar Opciones dentro de una Pregunta
 class OpcionInline(admin.TabularInline):
@@ -60,4 +60,3 @@ admin.site.register(DocumentoTransparencia)
 admin.site.register(ListaObligaciones)
 admin.site.register(ArticuloLiga)
 admin.site.register(LigaArchivo)
-admin.site.register(CarpetaTransparencia)
